fd70350c67372d77566b4cafd059a9fcb1c53c67
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-03-17 21:21:06 +0100

    Gradient

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,23 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import Data.List
+
+width = 300
+
+height = 200
+
+header = "P3\n" ++ show (width) ++ " " ++ show (height) ++ "\n255\n"
+
+color x y = show (x + y) ++ " 0 0"
+
+rows = map (\y -> intercalate " " $ map (\x -> color x y) [1..width]) [1..height]
+
+body = intercalate " " rows
+
+content = header ++ body
 
 someFunc :: IO ()
-someFunc = do 
-    writeFile "image.ppm" "P3\n2 2\n255\n255 0 0 255 0 255\n0 0 0 0 255 0\n"
+someFunc = do
+  writeFile "image.ppm" content
